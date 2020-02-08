3cce8a636c6be456bcea4500f58d05f3ca224062
naoshi.murayama <go_convination9@yahoo.co.jp>
2020-02-08 13:51:30 +0000

    time

--- app/models/dairy.rb
+++ app/models/dairy.rb
@@ -2,5 +2,5 @@ class Dairy < ApplicationRecord
   belongs_to :user
   
   validates :title, presence: true, length:{ maximum: 50 }
-  validates :content, presence: true, length: { maximum: 400 }
+  validates :content, presence: true, length: { maximum: 500 }
 end
